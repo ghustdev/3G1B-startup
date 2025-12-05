54ae235487009e0e940d7aa79a23927815c9ddfb
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-12-05 02:33:27 -0300

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit, Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
+import { Analytics } from '@vercel/analytics/next';
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -42,6 +43,7 @@ export default function RootLayout({
             {children}
           </main>
         </LanguageProvider>
+        <Analytics />
       </body>
     </html>
   );
